485bf0cc82d4a0958b3497c0ccfee72325e72779
NurAleeya <167388622+NurAleeya@users.noreply.github.com>
2024-07-24 10:37:39 +0800

    Update item.py

---
+++ ResourceCentre_23023624/inventory/item.py
@@ -0,0 +1,36 @@
+class Item():
+    def __init__(self, assertTag, description):
+        self.__assertTag = assertTag
+        self.__description = description
+        self._dueDate = ""
+        self.__isAvailable = True
+
+    def getAssetTag(self):
+        return self.__assertTag
+    
+    def getDescription(self):
+        return self.__description
+    
+    def getdueDate(self):
+        return self._dueDate
+    
+    def getIsAvailable(self):
+        if self.__isAvailable:
+            return "Yes"
+        else:
+            return "No"
+        
+    def getOpticalZoom(self):
+        return self.__opticalZoom
+    
+    def setDueDate(self, dueDate):
+        self._dueDate = dueDate
+
+    def setIsAvailable(self, isAvailable):
+        self.__isAvailable = isAvailable
+
+    def __str__(self):
+        return "{:<10}{:<30}{:<10}{:<12}".format(
+            self.getAssetTag(), self.getDescription(), 
+            self.getIsAvailable(), self.getdueDate(),
+            self.getOpticalZoom())
